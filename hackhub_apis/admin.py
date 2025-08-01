e9c3e75d29dd94a7767c71920ed832fdd55dc210
divyesh <rajjivani027@gmail.com>
2025-08-01 23:38:30 +0530

    Created CustomUserAdmin model

--- hackhub_apis/admin.py
+++ hackhub_apis/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email','username','full_name','is_staff')
+    list_filter = ('is_staff','is_superuser')
+    fieldsets = (
+        (None, {'fields': ('email','username','password')}),
+        ('Personal Info', {'fields': ('full_name')}),
+        ('Permissions',{'fields':('is_staff','is_superuser','is_active','groups','user_permissions')})
+    )
+
+    add_fieldsets = (
+        (None,{
+            'fields':('email','username','full_name','password1','password2'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('id')
+
+admin.site.register(CustomUser,CustomUserAdmin)
 # Register your models here.
