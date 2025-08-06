db65bc1e2613ded0058a34ad0e4ad20ab300a17e
divyesh <rajjivani027@gmail.com>
2025-08-06 23:59:21 +0530

    Created IsSameUserOrReadOnly permission for checking that is the current user is same as the CustomUser object for PATCH,PUT and DELETE type of requests

--- hackhub_apis/permissions.py
+++ hackhub_apis/permissions.py
@@ -6,3 +6,10 @@ class IsAuthorOrReadOnly(BasePermission):
             return True
         
         return obj.author == request.user
+    
+class IsSameUserOrReadOnly(BasePermission):
+    def has_object_permission(self,request,view,obj):
+        if request.method == SAFE_METHODS:
+            return True
+        
+        return obj == request.user
