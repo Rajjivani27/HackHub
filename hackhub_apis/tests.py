540c0e04e74dc8a5c200b3b5ca47ebd0c6faecb8
Rajjivani27 <rajjivani027@gmail.com>
2025-08-26 00:41:27 +0530

    Started learning tests and how to write test-cases

--- hackhub_apis/tests.py
+++ hackhub_apis/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from rest_framework.test import APIClient,APITestCase
+from django.urls import reverse
+from .models import *
+
+class PostTests(TestCase):
+    def test_post_listview(self):
+        response = self.client.get(reverse("posts-list"))
+        self.assertEqual(response.status_code,200)
 
 # Create your tests here.
