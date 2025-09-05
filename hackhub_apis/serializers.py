d18a712f5e80bd1a3654f5d563a42a6893f0e597
Rajjivani27 <rajjivani027@gmail.com>
2025-09-05 18:49:45 +0530

    Added some fields on CustomUserSerializer and PostSerializer and added Update method on PostSerializer and added Serializer for TeamFinder model as TeamOpportunitySerializer

--- hackhub_apis/serializers.py
+++ hackhub_apis/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-from .models import CustomUser,Profile,Post,PostMedia
+from .models import *
 from django.db import transaction
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ['name','user_bio','profile_pic','dob']
+        fields = ['name','user_bio','profile_pic','dob','university','github']
 
 class CustomUserSerializer(serializers.ModelSerializer):
     profile = ProfileSerializer() #Profile Serializer
@@ -41,17 +41,38 @@ class PostMediaSerializer(serializers.ModelSerializer):
         fields = ['files']
 
 class PostSerializer(serializers.ModelSerializer):
+    profile_pic = serializers.SerializerMethodField(read_only=True)
+    author = serializers.StringRelatedField(read_only = True)
+    author_username = serializers.SerializerMethodField(read_only=True)
+    likes_count = serializers.SerializerMethodField()
     media = PostMediaSerializer(many=True,write_only=True)
     github = serializers.CharField(write_only=True,required=False)
+
     class Meta:
         model = Post
-        fields = ['title','content','media','github','author']
+        fields = ['id','title','content','media','github','author','technologies','category','profile_pic','created_at','likes_count','author_username']
 
+    def get_author_username(self,obj):
+        request = self.context.get('request')
+        author = obj.author
+        return author.username
+
+    def get_likes_count(self,obj):
+        return obj.likes.count()
+
+    def get_profile_pic(self,obj):
+        request = self.context.get('request')
+        author = obj.author
+        if author.profile.profile_pic:
+            return request.build_absolute_uri(author.profile.profile_pic.url)
+        
     @transaction.atomic
     def create(self, validated_data):
         github = validated_data.pop('github')
         media = validated_data.pop('media')
         content = validated_data['content']
+
+        print(media)
 
         add = f"\nGithub Link : {github}"
 
@@ -60,8 +81,33 @@ class PostSerializer(serializers.ModelSerializer):
         post = Post.objects.create(**validated_data)
 
         media_objs = [PostMedia(post=post,files=item['files']) for item in media]
+        print(f"Media : {media_objs}")
         PostMedia.objects.bulk_create(media_objs)
         return post
+    
+    def update(self, instance, validated_data):
+        if validated_data['media']:
+            media = validated_data.pop('media')
+            print(f"Media : {media}")
+            for item in media:
+                print(item['files'])
+            media_objs = [PostMedia(post=instance,files=item['files']) for item in media]
+            print(f"Media : {media_objs}")
+            post = PostMedia.objects.bulk_create(media_objs)
+            print(f"Medias : {post}")
+
+        return instance
+    
+class TeamOpportunitySerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+    created_at = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = TeamFinder
+        fields = ['user','title','details','created_at','last_date']
+
+    def get_created_at(self,obj):
+        return obj.created_at
+
     
 class VerifyEmailSerializer(serializers.Serializer):
     uid = serializers.CharField()
